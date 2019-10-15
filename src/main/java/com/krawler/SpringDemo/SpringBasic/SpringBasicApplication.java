a53d5f1aacb9e6c841440156d7d2a37abeaf7a8f
keshav.singh <keshav.singh@deskera.org>
2019-10-15 18:48:41 +0530

    spring boot

--- src/main/java/com/krawler/SpringDemo/SpringBasic/SpringBasicApplication.java
+++ src/main/java/com/krawler/SpringDemo/SpringBasic/SpringBasicApplication.java
@@ -1,23 +1,17 @@
 package com.krawler.SpringDemo.SpringBasic;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
-@ComponentScan("com.krawler.SpringDemo")
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.*;
+
+@Configuration
 public class SpringBasicApplication {
 
 	public static void main(String[] args) {
 		
-//		NORMAL FLOW WITHOUT SPRING
-//		MergeSortAlgo sortAlgo = new MergeSortAlgo();
-//		BinarySearchImpl binarySearch = new BinarySearchImpl(sortAlgo);
-		
-		ApplicationContext appContext = SpringApplication.run(SpringBasicApplication.class, args);
+		ApplicationContext appContext = AnnotationConfigApplicationContext(SpringBasicApplication.class);
 		BinarySearchImpl binarySearch = appContext.getBean(BinarySearchImpl.class);
 		
-		binarySearch.binarySearch( new int[] {1,2,3,4}, 3);
+		
 				
 	}
 }
--- src/main/java/com/krawler/SpringDemo/SpringBasic/SpringScope.java
+++ src/main/java/com/krawler/SpringDemo/SpringBasic/SpringScope.java
@@ -1,15 +1,15 @@
 package com.krawler.SpringDemo.SpringBasic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 import com.krawler.SpringDemo.Scope.StudentDAO;
 import com.krawler.SpringDemo.Scope.StudentInfo;
 
-@SpringBootApplication
+@Configuration
 @ComponentScan("com.krawler.SpringDemo.Scope")
 public class SpringScope {
 	
@@ -19,7 +19,7 @@ public class SpringScope {
 		
 		
 		
-		ApplicationContext appContext = SpringApplication.run(SpringScope.class, args);
+		ApplicationContext appContext = AnnotationConfigApplicationContext(SpringScope.class, args);
 		
 		StudentInfo student = appContext.getBean(StudentInfo.class);
 		StudentDAO conn = appContext.getBean(StudentDAO.class);
